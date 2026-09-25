c32e2430b30ef928c7df39462daf0ca878526f04
Altiliano Fonseca <altiliano.fonseca@springernature.com>
2026-09-25 15:14:39 +0100

    Boot messaging API on SunHttp server

--- src/main/kotlin/messaging/Main.kt
+++ src/main/kotlin/messaging/Main.kt
@@ -1,4 +1,11 @@
 package messaging
 
+import messaging.app.MessagingApp
+import org.http4k.server.SunHttp
+import org.http4k.server.asServer
+
 fun main() {
+    val port = System.getenv("PORT")?.toIntOrNull() ?: 8080
+    val server = MessagingApp().http.asServer(SunHttp(port)).start()
+    println("messaging api listening on http://localhost:${server.port()}")
 }
